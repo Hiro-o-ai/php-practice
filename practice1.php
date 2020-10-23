0c1de422c0d4dcaa7c5af0004b80ef9a1119173b
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-10-23 16:42:36 +0900

    finish_progate_php

---
+++ practice1.php
@@ -0,0 +1,31 @@
+<!-- 道場編 -->
+<?php
+echo "Hello, PHP";
+echo "<br>";
+echo "10 + 7";
+?>
+
+<?php
+$name = 'Tom';
+echo '変数$nameの値: '.$name;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+echo "My name is ".$name;
+?>
+<?php
+$price = 1000;
+$taxRate = 0.08;
+echo '変数$priceの値: '.$price;
+echo '<br>';
+echo '変数$taxRateの値: '.$taxRate;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+$output =  $price + $price * $taxRate;
+echo "税込価格は".$output."円です"
+?>
