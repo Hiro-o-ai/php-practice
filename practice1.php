2f7f63eca12a1c487d5d55c7daee2991168abfee
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-10-26 17:12:01 +0900

    paiza_php_part2_cha2

--- practice1.php
+++ practice1.php
@@ -133,3 +133,34 @@ echo "賞金を分け合う人数は".$person."人です
 $a = $money / $person;
 echo "賞金を分け合うと一人当たり".$a."円になります。";
 ?>
+<?php
+$place = 1; // 何位かを$placeに数字で代入
+echo "placeの中身:".$place." \n";
+if(条件式){
+    // 1位だったときの処理
+    echo "優勝";
+}else{
+    // それ以外だったときの処理
+    echo $place."位";
+}
+?>
+<?php
+$num = 2;
+if($num == 1){
+    echo "ナンバーワン";
+}else{
+    echo "二位以下";
+}
+?>
+<?php
+$text = "勇者";
+echo "textの中身:".$text."\n";
+if($text == "勇者"){
+    // 勇者だった場合の処理
+    echo "ようこそ";
+}else{
+    // それ以外だったときの処理
+    echo "ここはあなたが来る場所ではありません";
+}
+?>
+
